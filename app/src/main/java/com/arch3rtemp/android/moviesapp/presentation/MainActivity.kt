830975c5f081a80e4d9a51fa5d6de4a414976177
archil <arch3r_temp@yahoo.com>
2022-09-02 23:38:06 +0400

    Initial commit

---
+++ app/src/main/java/com/arch3rtemp/android/moviesapp/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.arch3rtemp.android.moviesapp.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.arch3rtemp.android.moviesapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
